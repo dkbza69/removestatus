994cc12a142ad80e876bd988390a6c07d2d2be56
dkbza69 <arturobeltranasd@gmail.com>
2025-04-16 15:30:21 -0600

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Escuchando Spotify", "🎮 Jugando VALORANT"];
+const statusMessages = ["🧬 Proyecto de Umbrella Coorp.", "🧪 Módulo de control y respuesta de Umbrella Coorp."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
